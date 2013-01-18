bcbbe5ba06b5a450f1e222d86d2e7a7cfac407e7
Peter WONG <peter.wong@101medialab.com>
2013-01-18 16:53:36 +0800

    Refactor RssFormatter

--- Formatter/RssFormatter.php
+++ Formatter/RssFormatter.php
@@ -24,17 +24,16 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class RssFormatter extends Formatter implements FormatterInterface
 {
-    private $request;
+
+    private $channel;
 
     /**
      * Construct a formatter with given feed
      *
      * @param Feed $feed A feed instance
      */
-    public function __construct(Feed $feed, Request $request)
+    public function __construct(Feed $feed)
     {
-        $this->request = $request;
-
         $this->fields = array(
             new Field(
                 'title',
@@ -58,8 +57,6 @@ class RssFormatter extends Formatter implements FormatterInterface
         );
 
         parent::__construct($feed);
-
-        $this->initialize();
     }
 
     /**
@@ -81,36 +78,33 @@ class RssFormatter extends Formatter implements FormatterInterface
         $root->setAttribute('version', '2.0');
         $root = $this->dom->appendChild($root);
 
-        $channel = $this->dom->createElement('channel');
-        $channel = $root->appendChild($channel);
+        $this->channel = $this->dom->createElement('channel');
+        $this->channel = $root->appendChild($this->channel);
+    }
 
+    public function writeHeader($currentUrl)
+    {
         $fields = array('title', 'description', 'link');
 
         foreach ($fields as $field) {
             $element = $this->dom->createElement($field, $this->feed->get($field));
-            $channel->appendChild($element);
+            $this->channel->appendChild($element);
         }
 
         $element = $this->dom->createElement('atom:link');
-        $element->setAttribute('href', $this->request->getUri());
+        $element->setAttribute('href', $currentUrl);
         $element->setAttribute('rel', 'self');
         $element->setAttribute('type', 'application/rss+xml');
-        $channel->appendChild($element);
+        $this->channel->appendChild($element);
 
-        $channel->appendChild($this->dom->createElement('language', 'en-US'));
-        $channel->appendChild($this->dom->createElement('sy:updatePeriod', 'hourly'));
-        $channel->appendChild($this->dom->createElement('sy:updateFrequency', '1'));
+        $this->channel->appendChild($this->dom->createElement('language', 'en-US'));
+        $this->channel->appendChild($this->dom->createElement('sy:updatePeriod', 'hourly'));
+        $this->channel->appendChild($this->dom->createElement('sy:updateFrequency', '1'));
 
         $date = new \DateTime();
         $lastBuildDate = $this->dom->createElement('lastBuildDate', $date->format(\DateTime::RSS));
 
-        $channel->appendChild($lastBuildDate);
-
-        $items = $this->feed->getItems();
-
-        foreach ($items as $item) {
-            $this->addItem($channel, $item);
-        }
+        $this->channel->appendChild($lastBuildDate);
     }
 
     /**
@@ -119,10 +113,10 @@ class RssFormatter extends Formatter implements FormatterInterface
      * @param \DOMElement   $channel The channel DOM element
      * @param ItemInterface $item    An entity object
      */
-    public function addItem(\DOMElement $channel, ItemInterface $item)
+    public function addItem(ItemInterface $item)
     {
         $node = $this->dom->createElement('item');
-        $node = $channel->appendChild($node);
+        $node = $this->channel->appendChild($node);
 
         foreach ($this->fields as $field) {
             $element = $this->format($field, $item);
